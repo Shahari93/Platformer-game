d9700505fa05ae94333dbc1a0f3a35a08b8c8f6c
Shahari93 <shaharilani93@gmail.com>
2021-02-21 09:05:56 +0200

    More UI

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    [SerializeField] private PlayerCollision playerColl;
+    BoxCollider2D boxCollider;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (playerColl.hasKey)
+        {
+            boxCollider.isTrigger = true;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCollision : MonoBehaviour
 {
 
     public bool hasKey = false;
     public int coinAmount = 0;
+    public int lives = 3;
+    [SerializeField] private Image[] hearts;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            lives--;
+            hearts[lives].gameObject.SetActive(false);
+            if (lives <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
