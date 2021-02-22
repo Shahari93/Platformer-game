578f2a622d7c85cdb23e403783de5c96dd56e990
Shahari93 <shaharilani93@gmail.com>
2021-02-22 23:31:26 +0200

    Arrows

---
+++ Assets/Scripts/Player/Arrow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    Rigidbody2D arrowRB;
+    BoxCollider2D arrowColl;
+    bool hasHits;
+    private void Start()
+    {
+        arrowRB = GetComponent<Rigidbody2D>();
+        arrowColl = GetComponent<BoxCollider2D>();
+    }
+
+    //private void Update()
+    //{
+    //    if (!hasHits)
+    //    {
+    //        float angle = Mathf.Atan2(arrowRB.velocity.y, arrowRB.velocity.x) * Mathf.Rad2Deg;
+    //        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    //    }
+    //}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hasHits = true;
+        arrowRB.velocity = Vector2.zero;
+        arrowRB.isKinematic = true;
+        arrowColl.isTrigger = true;
+    }
+}
