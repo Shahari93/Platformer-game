01b6c2ca7ac0e52725e0747802ecde745fb6f2b7
Shahari93 <shaharilani93@gmail.com>
2021-02-28 22:09:40 +0200

    Working animations
    Idle running jumping and falling

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,25 +4,82 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D playerRB = null;
-    [SerializeField] private SpriteRenderer playerSR = null;
-    [SerializeField] private Animator playerAnimator = null;
+    //Init variables
+    private Rigidbody2D playerRB = null;
+    private SpriteRenderer playerSR = null;
+    private Animator playerAnimator = null;
+    private Collider2D playerColl = null;
+
+    [Header("Variables")]
+    [SerializeField] private LayerMask ground = 0;
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+
+    //FSM
+    private enum State { idle, running, jumping, fall }
+    private State state = State.idle;
+
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerSR = GetComponent<SpriteRenderer>();
+        playerAnimator = GetComponent<Animator>();
+        playerColl = GetComponent<Collider2D>();
+    }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        Movement();
+
+        AnimationSwitchStates();
+        playerAnimator.SetInteger("State", (int)state); // sets animations based on enum states
+    }
+
+    private void Movement()
+    {
+        float horMov = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        if (horMov < 0)
         {
-            playerRB.velocity = new Vector2(-5f, playerRB.velocity.y);
+            playerRB.velocity = new Vector2(-moveSpeed, playerRB.velocity.y);
             playerSR.flipX = true;
+
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (horMov > 0)
         {
-            playerRB.velocity = new Vector2(5f, playerRB.velocity.y);
+            playerRB.velocity = new Vector2(moveSpeed, playerRB.velocity.y);
             playerSR.flipX = false;
+
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetButtonDown("Jump") && playerColl.IsTouchingLayers(ground))
         {
-            playerRB.velocity = new Vector2(playerRB.velocity.x, 10f);
-        }    
+            playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+            state = State.jumping;
+        }
+    }
+
+    private void AnimationSwitchStates()
+    {
+        if (state == State.jumping)
+        {
+            if (playerRB.velocity.y < .1f)
+            {
+                state = State.fall;
+            }
+        }
+        else if (state == State.fall)
+        {
+            if (playerColl.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if (Mathf.Abs(playerRB.velocity.x) > 2f)
+        {
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
     }
 }
