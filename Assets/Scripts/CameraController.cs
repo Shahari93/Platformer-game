3f9fc4526f8292dcadfd8ac70ff7a0e772b99652
Shahari93 <shaharilani93@gmail.com>
2021-02-28 18:36:46 +0200

    Start of the game
    Movement
    Jumping
    Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform transformToFollow;
+
+    private void Update()
+    {
+        transform.position = new Vector3(transformToFollow.position.x, transformToFollow.position.y, transform.position.z);
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Rigidbody2D playerRB = null;
+    [SerializeField] private SpriteRenderer playerSR = null;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRB.velocity = new Vector2(-5f, playerRB.velocity.y);
+            playerSR.flipX = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerRB.velocity = new Vector2(5f, playerRB.velocity.y);
+            playerSR.flipX = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.velocity = new Vector2(playerRB.velocity.x, 10f);
+        }    
     }
 }
